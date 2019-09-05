8f716ad5edf5aa377ed4f08e65fd6e4bbd78df25
Zaya <zayazaw67@gmail.com>
2019-09-04 21:42:32 -0500

    added wins losses lives variables. added onkeyup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,14 @@
 var musicians = ["Lady Gaga", "Taylor Swift", "Miley Cyrus", "Katy Perry",
-"Eminem", "Drake", "John Mayer", "Pitbull"]
+"Eminem", "Drake", "John Mayer", "Pitbull", "Chris Brown", "Khalid", "Post Malone", "Ariana Grande"]
+var guesses = ["a", "b", "c"]
+
+var wins = 0;
+var losses = 0;
+var lives = 10;
 
 var computerSelection = musicians[Math.floor(Math.random() * musicians.length)];
 console.log(computerSelection)
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    console.log(userGuess);}
