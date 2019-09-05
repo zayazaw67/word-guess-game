f1e788965210bd0b4dd8625b0f897dabbdb3e39a
Zaya <zayazaw67@gmail.com>
2019-09-04 21:57:41 -0500

    added letters to be included in userguess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var musicians = ["Lady Gaga", "Taylor Swift", "Miley Cyrus", "Katy Perry",
-"Eminem", "Drake", "John Mayer", "Pitbull", "Chris Brown", "Khalid", "Post Malone", "Ariana Grande"]
-var guesses = ["a", "b", "c"]
+    "Eminem", "Drake", "John Mayer", "Pitbull", "Chris Brown", "Khalid", "Post Malone", "Ariana Grande"]
+var guesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+    "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
 
 var wins = 0;
 var losses = 0;
@@ -9,6 +10,8 @@ var lives = 10;
 var computerSelection = musicians[Math.floor(Math.random() * musicians.length)];
 console.log(computerSelection)
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     var userGuess = event.key;
-    console.log(userGuess);}
+    if (guesses.includes(userGuess))
+        console.log("correct");
+}
