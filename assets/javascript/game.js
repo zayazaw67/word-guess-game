4ba9224486891ad30175a650e3643afe0ce64b50
Zaya <zayazaw67@gmail.com>
2019-09-06 20:37:17 -0500

    made more variables, broken down word's letter's into single elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,39 +3,49 @@ var musicians = ["Lady Gaga", "Taylor Swift", "Miley Cyrus", "Katy Perry",
 var validLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
     "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
 
+    var wins = 0;
+    var losses = 0;
+    var guessesLeft = 5;
+    var guessedLetters = [];
+    var answerBlanks = [];
+    var regex = /[a-z]/;
+
+    var winsText = document.getElementById("wins-text")
+    var lossesText = document.getElementById("losses-text")
+    var guessesLeftText = document.getElementById("guessesleft-text")
+    var guessedLettersText = document.getElementById("guessedletters-text")
 
 
-var wins = 0;
-var losses = 0;
-var lives = 10;
 
-// var userChoiceText = document.getElementById("userchoice-text");
-// var winsText = document.getElementById("wins-text");
-// var lossesText = document.getElementById("losses-text");
-var answerText = document.getElementById("answer-text");
+
+
+
 
 // choose a word
 var computerSelection = musicians[Math.floor(Math.random() * musicians.length)];
 console.log(computerSelection)
 
-var answer = [];
-    for (var i=0; i <computerSelection.length; i++) {
-        console.log(answer[i] ="_");
-    }
+// separate selected word to make each letter an element
+wordLetters = computerSelection.split("");
+console.log(wordLetters)
+
+
+blanks = wordLetters.length;
+    console.log(blanks)
 
 // var remainingLetters = computerSelection.length
 
 // user input
-document.onkeyup = function(event) {
-    var userGuess = event.key; 
-    console.log (userGuess);
-    if (validLetters.includes(userGuess))
-    alert("working")}
-        
-
-
-
-
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+    var userChoice = event.key;
+                                //   if (!regex.test(userChoice)) {
+                                //     console.log("please enter a letter");}
+                                //   }
+    if (userGuess = computerSelection) {
+    }
+}
 
 
 
